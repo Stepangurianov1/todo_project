0136861865d388ad43e18fefe7107c82420d9c2e
stepa <stepan.gurianov@gmail.com>
2022-12-10 12:34:11 +0300

    lesson9

--- todo/urls.py
+++ todo/urls.py
@@ -17,15 +17,28 @@ from django.contrib import admin
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-
 from users.views import UsersModelViewSet, UsersAPIView
 
 from work.views import ProjectModelViewSet, TodoModelViewSet
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='librar',
+        default_version='v1',
+        description='Test api',
+        contact=openapi.Contact(email='admin@gmail.com'),
+        license=openapi.License(name='license'),
+    ),
+    public=True,
+    # permission_classes=(Allowany)
+)
 
 router = DefaultRouter()
 
-router.register('users', UsersModelViewSet)
+# router.register('users', UsersModelViewSet)
 router.register('project', ProjectModelViewSet)
 router.register('todo', TodoModelViewSet)
 urlpatterns = [
@@ -33,6 +46,9 @@ urlpatterns = [
     path('api-token-auth/', views.obtain_auth_token),
     # path('api-todo/', TodoAPIView.as_view()),
     path('admin/', admin.site.urls),
+    path('swagger<str:format>/', schema_view.without_ui()),
+    path('swagger/', schema_view.with_ui('swagger')),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
+    path('api/<str:version>/users/', UsersModelViewSet.as_view({'get': 'list'}))
 ]
--- users/serializers.py
+++ users/serializers.py
@@ -6,3 +6,9 @@ class UsersModelSerializer(ModelSerializer):
     class Meta:
         model = MyUsers
         fields = ('id', 'email', 'first_name', 'last_name', 'username')
+
+
+class UserV2ModelSerializer(ModelSerializer):
+    class Meta:
+        model = MyUsers
+        fields = ('is_active', 'is_superuser')
